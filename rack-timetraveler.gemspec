7f4d66ada286fdb0dfa91bf00c60f3bf51ba1d31
YutaYoshiike <yuta.yoshiike@mixi.co.jp>
2016-03-24 16:28:08 +0900

    Add coverage tools

--- rack-timetraveler.gemspec
+++ rack-timetraveler.gemspec
@@ -25,4 +25,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rack-test'
   spec.add_development_dependency 'rspec', '~>3.3'
   spec.add_development_dependency 'coveralls'
+  spec.add_development_dependency 'simplecov'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,21 @@ ENV['RACK_ENV'] = 'test'
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rack/test'
 require 'rspec'
-require 'rack/timetraveler'
 
+require 'simplecov'
 require 'coveralls'
 Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start do
+    add_filter '.bundle/'
+    add_filter 'vendor'
+end
+
+require 'rack/timetraveler'
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
